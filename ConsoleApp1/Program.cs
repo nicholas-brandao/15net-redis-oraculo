d8ecbcd2eeabb3778110222a6dfbaac5f754c72a
Logon Aluno <logonaluno@local.com>
2019-08-07 21:45:20 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("40.77.24.62");
+
+            //IDatabase db = connection.GetDatabase();
+            //// Get/Set
+            //db.StringSet("abc", "1");
+            //var value = db.StringGet("abc");
+
+            var pub = connection.GetSubscriber();
+
+            var sub = connection.GetSubscriber();
+            
+            sub.Subscribe("Perguntas", (ch, msg) =>
+            {
+
+                if (msg == "Qual a capital do Brasil ?")
+                {
+                    IDatabase redis = connection.GetDatabase();
+                    redis.HashSet("P1", "Nicholas", "Brasilia");
+                }
+                Console.WriteLine(msg.ToString());
+            });
+
+            Console.ReadLine();
+            //while (true)
+            //{
+            //    Console.WriteLine();
+            //    string linha = Console.ReadLine();
+            //    pub.Publish("net15", "Hello World");
+            //}
+
+
+        }
+    }
+}
